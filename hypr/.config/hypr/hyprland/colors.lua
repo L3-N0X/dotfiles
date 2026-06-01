7180ba8a31da27a43de8a4b4ec5ad985f9e3f06d
L3-N0X <leon.goett@web.de>
2026-06-01 18:02:37 +0200

    Added matugen for vesktop

--- hypr/.config/hypr/hyprland/colors.lua
+++ hypr/.config/hypr/hyprland/colors.lua
@@ -1,16 +1,16 @@
 hl.config({
     general = {
         col = {
-            active_border   = "rgba(53443d77)",
-            inactive_border = "rgba(231a1633)",
+            active_border   = "rgba(52434277)",
+            inactive_border = "rgba(23191933)",
         },
     },
     misc = {
-        background_color = "rgba(1a110eFF)",
+        background_color = "rgba(1a1111FF)",
     },
 })
 
 hl.window_rule({
     match        = { pin = 1 },
-    border_color = "rgba(ffb596AA) rgba(ffb59677)",
+    border_color = "rgba(ffb3b1AA) rgba(ffb3b177)",
 })
